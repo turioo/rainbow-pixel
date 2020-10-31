a2bc77156dbd12d5bfe3fa0a28e5088382c5f3bd
turioo <73040986+turioo@users.noreply.github.com>
2020-10-31 19:51:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,23 +15,27 @@ function App(props) {
     let pixelsArr = props.state.map( p => <Pixel id={p.id} backColor={p.backColor} changeColor={props.changeColor} takeColor={takeColor}/>)
     spaceArr = props.whitePixels.map( p => <WhitePixels id={p.id} backColor={p.backColor} takeColor={currentColor} />)
 
-    function saveimg(){
-   
-    }
+
   
   return (
     <div className='app-wrapper'>
-      <div className='header'>
+      <div className='pageLoad'>
+
+        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div> 
+        <div>Please wait</div>
+      </div>
+      
+      {/* <div className='header'>
         <img src='https://i.imgur.com/yPd9FdW.png' alt='pines'></img>
         <div className='colors'>{pixelsArr}</div>
         <div className='button' onClick={() => window.location.reload(false)}>Clear</div>
       </div>
       <div className='artBody'>
         <div id='mycanvas' className='place'>{spaceArr}</div>
-        <div className='button' onClick={saveimg()}>Download</div>
+        <div className='button'>Download</div>
         <div className='text'>Turio created. Ⓒ 2020</div>
       
-      </div>
+      </div> */}
      </div>
   );
 }
